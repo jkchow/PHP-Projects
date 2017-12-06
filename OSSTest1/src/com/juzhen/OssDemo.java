4624d80ce732da88caf91914cc9ad7edcf0760e9
jackchow <jkzzit@163.com>
2017-12-06 11:33:39 +0800

    OSSTest1

--- OSSTest1/src/com/juzhen/OssDemo.java
+++ OSSTest1/src/com/juzhen/OssDemo.java
@@ -12,8 +12,6 @@ public class OssDemo {
 
 
 
-//asjdkf
-
 	public static void main(String[] args) {
 		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 		// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建
